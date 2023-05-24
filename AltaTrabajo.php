e17393a4f51dd26ce86e3503f96152326db71c3b
EJPZZZ <ej74519@gmail.com>
2023-05-23 18:14:50 -0600

    Pagina web para una tienda de abarrotes hecha con HTML5, PHP y CSS

--- AltaTrabajo.php
+++ AltaTrabajo.php
@@ -16,6 +16,6 @@ echo "$nombre, $edad, $trabajo, $escuela, $domicilio, $numero";
 var_dump('check');
 $conectar = conn(); //ejecuta las conexiones a la bd
 $sql = "INSERT INTO work (nombre, edad, trabajo, escuela, domicilio, numero) 
-        VALUES ('$nombre', '$edad', '$trabajo' '$escuela', '$domicilio', '$numero')";
+        VALUES ('$nombre', '$edad', '$trabajo', '$escuela', '$domicilio', '$numero')";
 $result = mysqli_query($conectar, $sql) or trigger_error("Query Failed! SQL-Error:" . mysqli_error($conectar), E_USER_ERROR);
 echo "$sql";
